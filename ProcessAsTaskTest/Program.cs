2cfe4ab8360c99e702672effbc77686c8088112d
aross <aross@AROSS>
2018-01-18 15:45:21 +0000

    Add project files.

---
+++ ProcessAsTaskTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using RunProcessAsTask;
+
+namespace ProcessAsTaskTest
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var processResults = ProcessEx.RunAsync("ping.exe", "www.bbc.co.uk").Result;
+
+      Console.WriteLine("Exit code: " + processResults.ExitCode);
+      Console.WriteLine("Run time: " + processResults.RunTime);
+
+      Console.WriteLine("{0} lines of standard output", processResults.StandardOutput.Length);
+      foreach (var output in processResults.StandardOutput)
+      {
+        Console.WriteLine("Output line: " + output);
+      }
+
+      Console.WriteLine("{0} lines of standard error", processResults.StandardError.Length);
+      foreach (var error in processResults.StandardError)
+      {
+        Console.WriteLine("Error line: " + error);
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
