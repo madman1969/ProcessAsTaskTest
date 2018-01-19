b53f02acae0429b5921c0bb23e2ad4b2c896fad5
Andrew Ross <aross69@gmail.com>
2018-01-19 09:51:36 +0000

    + Added support for Colorful.Console NuGet package. + Minor refactoring.

---
+++ ProcessAsTaskTest/InvokePing.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using RunProcessAsTask;
+using Console = Colorful.Console;
+
+namespace ProcessAsTaskTest
+{
+  public class Invoke
+  {
+    private readonly Color stdOutColor = Color.GreenYellow;
+    private readonly Color defaultColor = Color.Aqua;
+    private readonly Color stdErrColor = Color.Red;
+
+    public void Ping(string url)
+    {
+      var processResults = ProcessEx.RunAsync("ping.exe", url).Result;
+
+      Console.WriteLine("Exit code: " + processResults.ExitCode, defaultColor);
+      Console.WriteLine("Run time: " + processResults.RunTime, defaultColor);
+
+      Console.WriteLine("{0} lines of standard output", processResults.StandardOutput.Length, stdOutColor);
+      foreach (var output in processResults.StandardOutput)
+      {
+        Console.WriteLine("Output line: " + output, stdOutColor);
+      }
+
+      Console.WriteLine("{0} lines of standard error", processResults.StandardError.Length, stdErrColor);
+      foreach (var error in processResults.StandardError)
+      {
+        Console.WriteLine("Error line: " + error, stdErrColor);
+      }
+    }
+  }
+}
--- ProcessAsTaskTest/Program.cs
+++ ProcessAsTaskTest/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Drawing;
 using RunProcessAsTask;
+using Colorful;
+
+using Console = Colorful.Console;
 
 namespace ProcessAsTaskTest
 {
@@ -7,24 +11,10 @@ namespace ProcessAsTaskTest
   {
     static void Main(string[] args)
     {
-      var processResults = ProcessEx.RunAsync("ping.exe", "www.bbc.co.uk").Result;
-
-      Console.WriteLine("Exit code: " + processResults.ExitCode);
-      Console.WriteLine("Run time: " + processResults.RunTime);
-
-      Console.WriteLine("{0} lines of standard output", processResults.StandardOutput.Length);
-      foreach (var output in processResults.StandardOutput)
-      {
-        Console.WriteLine("Output line: " + output);
-      }
-
-      Console.WriteLine("{0} lines of standard error", processResults.StandardError.Length);
-      foreach (var error in processResults.StandardError)
-      {
-        Console.WriteLine("Error line: " + error);
-      }
+      var tmp = new Invoke();
+      tmp.Ping("www.bbc.co.uk");
 
       Console.ReadLine();
-    }
+    }    
   }
 }
