8e7a6cbbcf85eea4dccbc0ed53c66322f6079e39
Andrew Ross <aross69@gmail.com>
2018-02-27 22:33:39 +0000

    + Re-orientated solution around CC65. + Removed redundant source files. + Separated solution into console app & class library.

---
+++ CC65/cc65Process.cs
@@ -0,0 +1,117 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using RunProcessAsTask;
+
+namespace CC65
+{
+  public class CC65Process
+  {
+    #region Fields and properties
+
+    public string WorkingDirectory { get; set; }
+    public string TargetPlatform { get; set; }
+    public string Compiler { get; private set; }
+    public string SourceFile { get; private set; }
+    public bool IsVerbose { get; set; }
+    public string Arguments { get; set; }
+    public ProcessResults Results { get; private set; }
+
+    public readonly Color StdOutColor = Color.GreenYellow;
+    public Color DefaultColor = Color.Aqua;
+    public Color StdErrColor = Color.Red;
+
+    #endregion
+
+    #region Constructor
+
+    public CC65Process()
+    {
+      // Set properties to defaults ...
+      WorkingDirectory = string.Empty;
+      TargetPlatform = "pet";
+      Compiler = "cl65.exe";
+      SourceFile = string.Empty;
+      IsVerbose = true;
+      Arguments = string.Empty;
+      Results = null;
+    }
+
+    #endregion 
+
+    #region Public Methods
+
+    public async Task<int> Compile(string sourceFile)
+    {
+      // Bail if hooky file ...
+      if (string.IsNullOrEmpty(sourceFile) ||
+          !File.Exists(sourceFile))
+        return -1;
+
+      //
+      // Setup compile process ...
+      //
+      SetWorkingDirectory(sourceFile);
+      SetSourceFile(sourceFile);
+      SetArguments();
+
+      var processStartInfo = new ProcessStartInfo
+      {
+        FileName = Compiler,
+        Arguments = Arguments,
+        WorkingDirectory = WorkingDirectory
+      };
+
+      // Run compilation as async process ...
+      Results = await ProcessEx.RunAsync(processStartInfo);
+
+      return Results.ExitCode;
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Set the working directory for the compilation process.
+    /// 
+    /// N.B. Only set if no existing value
+    /// 
+    /// </summary>
+    /// <param name="sourceFile"></param>
+    private void SetWorkingDirectory(string sourceFile)
+    {
+      if (string.IsNullOrEmpty(WorkingDirectory))
+      {
+        WorkingDirectory = Path.GetDirectoryName(sourceFile);
+      }
+    }
+
+    /// <summary>
+    /// Set the source file for the compilation process.
+    /// 
+    /// N.B. Only set if no existing value.
+    /// 
+    /// </summary>
+    /// <param name="sourceFile"></param>
+    private void SetSourceFile(string sourceFile)
+    {
+      if (string.IsNullOrEmpty(SourceFile))
+      {
+        SourceFile = Path.GetFileName(sourceFile);
+      }
+    }
+
+    /// <summary>
+    /// Set the arguments for the compilation process.
+    /// </summary>
+    private void SetArguments()
+    {
+      var verboseSetting = IsVerbose ? "--verbose" : string.Empty;
+      Arguments = $"{verboseSetting} -t {TargetPlatform} {SourceFile}";
+    }
+
+    #endregion 
+  }
+}
--- ProcessAsTaskTest/Program.cs
+++ ProcessAsTaskTest/Program.cs
@@ -1,17 +1,39 @@
-﻿using System;
+﻿using CC65;
+using Console = Colorful.Console;
 
-namespace ProcessAsTaskTest
+namespace CC65Console
 {
   class Program
   {
     static void Main(string[] args)
     {
-      //var tmp = new PingProcess();
-      //tmp.InvokeAsync("www.bbc.co.uk");
-
       var sourceFile = @"D:\Commodore Stuff\PET Stuff\csource\Draw\draw.c";
-      var tmp = new cc65Process();
-      tmp.InvokeAsync(sourceFile);
+      var process = new CC65Process();
+      var tmp = process.Compile(sourceFile).Result;
+
+      // Show exit code & run time ...
+      Console.WriteLine("Exit code: " + process.Results.ExitCode, process.DefaultColor);
+      Console.WriteLine("Run time: " + process.Results.RunTime, process.DefaultColor);
+
+      // 
+      // Dump StdOut ...
+      //
+      Console.WriteLine("{0} lines of standard output", process.Results.StandardOutput.Length, process.StdOutColor);
+
+      foreach (var output in process.Results.StandardOutput)
+      {
+        Console.WriteLine("Output line: " + output, process.StdOutColor);
+      }
+
+      //
+      // Dump StdErr ...
+      //
+      Console.WriteLine("{0} lines of standard error", process.Results.StandardError.Length, process.StdErrColor);
+
+      foreach (var error in process.Results.StandardError)
+      {
+        Console.WriteLine("Error line: " + error, process.StdErrColor);
+      }
 
       Console.ReadLine();
     }    
