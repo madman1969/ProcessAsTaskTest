53640ad47ad5e6711bc0150417b5b629b445dba5
Andrew Ross <aross69@gmail.com>
2018-02-27 23:04:00 +0000

    + Added test project. + Added some basic tests.

---
+++ CC65.Test/BasicCC65Tests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RunProcessAsTask;
+using Shouldly;
+
+namespace CC65.Test
+{
+  [TestClass]
+  public class BasicCC65Tests
+  {
+    [TestMethod]
+    public void CreateCC65ProcessShouldSucceed()
+    {
+      // Arrange ...
+
+      // Act ...
+      var process = new CC65Process();
+
+      // Assert ...
+      process.ShouldNotBeNull();
+      process.WorkingDirectory.ShouldBeNullOrEmpty();
+      process.Compiler.ShouldBe("cl65.exe");
+      process.SourceFile.ShouldBeNullOrEmpty();
+      process.IsVerbose.ShouldBeTrue();
+      process.Arguments.ShouldBeNullOrEmpty();
+      process.Results.ShouldBeNull();
+    }
+
+    [TestMethod]
+    public void CompileWithInvalidSourceFileShouldFailAsync()
+    {
+      // Arrange ...
+      var invalidSourceFile = "sdfsdfsdf";
+
+      // Act ...
+      var process = new CC65Process();
+      var resultCode = process.Compile(invalidSourceFile).Result;
+
+      // Assert ...
+      process.ShouldNotBeNull();
+      process.Results.ShouldBeNull();
+      resultCode.ShouldBe(-1);      
+    }
+  }
+}
---
+++ CC65.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CC65.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CC65.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67abe662-ee55-4d00-a721-f7061211a1b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
